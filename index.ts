2a894b746299d6bdb593430ca1ee6bd913994d0b
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-03-11 21:42:42 -0300

    add Tsconfig

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const input1 = document.getElementById("num1") as HTMLInputElement;
 const input2 = document.getElementById("num2") as HTMLInputElement;
-const button = document.getElementById("button");
+const button = document.getElementById("button")!;
 
 function sum(a: number, b: number) {
     return a + b;
@@ -9,3 +9,4 @@ function sum(a: number, b: number) {
 button.addEventListener("click", function () {
     console.log(sum(Number(input1.value), Number(input2.value)));
 });
+
