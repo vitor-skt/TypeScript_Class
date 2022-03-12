4e59cc8c0b9ec61831a62daeb991b53e55fef7df
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-03-11 21:53:25 -0300

    add types.ts

---
+++ types.ts
@@ -0,0 +1,23 @@
+//boolean (true / false)
+let isOpen: boolean
+isOpen = true
+
+// string ('text', "text", `text`)
+let message: string
+message = `text ${isOpen}`
+
+//number (int , float, hex, binary)
+let total: number
+total = 0xff0
+
+// array (type[])
+let items: number[]
+items = [1, 2, 3]
+
+let items2: string[]
+items2 = ['text', 'text', 'text']
+
+//or 
+
+let values: Array<number>
+values = [1, 2, 3]
