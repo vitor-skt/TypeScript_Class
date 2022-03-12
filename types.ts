60f369b80aae5c4bde6c8907455d055ae72e25a8
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-03-11 22:07:02 -0300

    types

--- types.ts
+++ types.ts
@@ -21,3 +21,40 @@ items2 = ['text', 'text', 'text']
 
 let values: Array<number>
 values = [1, 2, 3]
+
+//tuple 
+let title: [number, string, boolean]
+title = [1, 'text', true]
+
+// enum
+enum colors {
+    white = '#fff',
+    black = '#000',
+}
+
+// any (anything) not recomended!
+let thing: any
+thing = true
+thing = "text"
+thing = [1]
+
+// void (function without return)
+function logger() {
+    console.log('text')
+}
+
+// null / undefined
+let variable: null //don't do it!
+
+type variable = string | undefined //Better option
+
+//never
+function error(): never {
+    throw new Error("error");
+}
+
+// object
+let cart: object
+cart = {
+    key: 'text'
+}
