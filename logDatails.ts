fe55290479f1f56fb852da8d019a6c5390558081
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-03-13 16:38:49 -0300

    Parte 3.1

--- logDatails.ts
+++ logDatails.ts
@@ -1,6 +1,5 @@
-// Union and Aliases
-
-type Uid = number | string;
+// Type Alias
+type Uid = number | string | undefined;
 
 function logDetails(uid: Uid, item: string) {
     console.log(`A product with ${uid} has title as ${item}.`);
@@ -10,6 +9,13 @@ function logInfo(uid: Uid, user: string) {
     console.log(`An user with ${uid} has a name as ${user}.`);
 }
 
+type Platform = 'Windows' | 'Linux' | 'Mac Os' | 'Ios';
+
+function renderPlatform(platform: Platform) {
+    return platform
+}
+
+renderPlatform('Ios');
 
 logDetails(123, "shoes");
 logDetails("123", "shoes");
