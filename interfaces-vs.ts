316c5dfc7435a7de1c99ea0922a19b1f31acb660
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-03-16 12:28:44 -0300

    Type Alias vs Interface end

--- interfaces-vs.ts
+++ interfaces-vs.ts
@@ -19,3 +19,34 @@ class CreateGame implements GameCollection { }
 interface getSimilars {
     (title: string): void;
 }
+
+
+// ======== Differences ======== //
+
+interface ID extends number { }
+
+// Não é possivel definir tuplas na interface
+interface Tuple {
+    0: number;
+    1: number;
+}
+[1, 2, 3, 4, "sass"] as Tuple;
+
+// Pode ter multiplas declarações com o mesmo nome que ele une
+
+interface JQuery {
+    a: string;
+}
+
+interface JQuery {
+    b: string;
+}
+
+const $: JQuery = {
+    a: "bla",
+    b: "foo",
+};
+
+// Vantagem: quando tiver criando libs, prefira interfaces, pois são mais extensiveis!
+
+// criado objetos/classes (POO)
--- types-vs.ts
+++ types-vs.ts
@@ -11,12 +11,36 @@ type DLCT = {
 
 // intersection
 
-type GameCollectionT = Game & DLCT;
+type GameCollectionT = GameT & DLCT & { content: boolean };
 
 // implements 
 
-class CreateGameT implements GameCollection { }
+
+class CreateGameT implements GameCollectionT { }
 
 // function declaration 
 
 type getSimilarsT = (title: string) => void;
+
+// ======== Differences ======== //
+
+// Permite declarar tipos primitivos
+
+type IDT = string | number;
+
+// Pode declarar tuplas normalmente
+
+type TupleT = [number, number];
+
+[1, 2] as TupleT;
+
+// Apenas uma declaração por escopo
+
+type JQueryT = { a: string };
+// type JQueryT = {b: string}; //errado
+
+// Mais recomendado: na maioria das vezes (só caso tenha necessidade use interface)
+
+// React - Props
+
+// Consistência! (depende do que o projeto já esteja usando)
